9dcc9e0051cc15470a592c9c731e71da834db727
oxalica <oxalicc@pm.me>
2021-05-20 17:59:03 +0800

    Use direnv

--- flake.nix
+++
@@ -1,9 +0,0 @@
-{
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
-
-  outputs = { self, nixpkgs }: {
-    devShell.x86_64-linux = import ./shell.nix {
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    };
-  };
-}
